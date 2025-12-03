e17ece36822b1cf5a3963219ddd5d57178104fe2
CYX22222003 <e1155533@u.nus.edu>
2025-12-03 09:53:02 +0800

    Trimming body

--- src/StringUtils.hpp
+++ src/StringUtils.hpp
@@ -23,4 +23,11 @@ namespace StringUtils {
         }
         return result;
     }
+
+    std::string trim(const std::string& s) {
+        size_t start = s.find_first_not_of(" \t\r\n");
+        size_t end   = s.find_last_not_of(" \t\r\n");
+        if (start == std::string::npos) return "";
+        return s.substr(start, end - start + 1);
+    }
 }
--- src/handler.hpp
+++ src/handler.hpp
@@ -19,13 +19,13 @@ namespace handler {
             httpResponse.set_status(HttpStatus::OK);
         } else if (std::regex_match(target, match, rules::echo_r)) {
             std::string body = match[1];
-            httpResponse.write_text_plain(body);
+            httpResponse.write_text_plain(StringUtils::trim(body));
         } else if (target == "/user-agent") {
             auto headers = req.headers;
             auto it = headers.find("User-Agent");
             if (it == headers.end()) throw std::runtime_error("Does not contain User-Agent body");
             std::string body = it->second;
-            httpResponse.write_text_plain(body);
+            httpResponse.write_text_plain(StringUtils::trim(body));
         } else {
             httpResponse.set_status(HttpStatus::NotFound);
         }
